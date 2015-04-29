6a6b9b0a5cbae8e5554f09601a75be00706c3b70
Jeff Beorse <jbeorse@cs.washington.edu>
2015-04-29 15:47:33 -0700

    Made clearer where to set the paths to the SDK and NDK

--- local.env.mk
+++ local.env.mk
@@ -1,4 +1,4 @@
 #location of android-opencv port of OpenCV to android
 #OPENCV_CONFIG=android-opencv.mk
-ANDROID_NDK_ROOT=~/NVPACK/android-ndk-r8d-master
+ANDROID_NDK_ROOT=<PATH-TO-NDK>
 ARM_TARGETS="armeabi armeabi-v6 armeabi-v7a"
