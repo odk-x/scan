854e6bb6f4dca14590a781e721d4ae3b57f36bc3
linl33 <linlee1997@gmail.com>
2015-11-30 04:51:52 -0800

    Execute RunSetup with AsyncTask instead of Thread

--- scan_app/src/main/java/org/opendatakit/scan/android/activities/MainMenuActivity.java
+++ scan_app/src/main/java/org/opendatakit/scan/android/activities/MainMenuActivity.java
@@ -16,6 +16,7 @@ package org.opendatakit.scan.android.activities;
 
 import java.io.File;
 
+import android.os.*;
 import android.text.Html;
 import org.opendatakit.common.android.activities.BaseActivity;
 import org.opendatakit.scan.android.R;
@@ -28,10 +29,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
-import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.os.Message;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
@@ -70,9 +67,7 @@ public class MainMenuActivity extends BaseActivity {
                }
             });
 
-            Thread thread = new Thread(
-                new RunSetup(handler, settings, getAssets(), appVersionCode));
-            thread.start();
+            AsyncTask.execute(new RunSetup(handler, settings, getAssets(), appVersionCode));
          }
       } catch (Exception e) {
          // Display an error dialog if something goes wrong.
