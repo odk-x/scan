45cdfbc241f9562f934ac2aab4e19af4323a8d30
Jeff Beorse <jbeorse@cs.washington.edu>
2016-01-12 16:18:06 -0800

    Allow local environment NDK path

--- bubblebot_lib/local.env.mk
+++ bubblebot_lib/local.env.mk
@@ -1,4 +1,4 @@
 #location of android-opencv port of OpenCV to android
 #OPENCV_CONFIG=android-opencv.mk
-ANDROID_NDK_ROOT=<PATH-TO-NDK>
+ANDROID_NDK_ROOT=$(ANDROID_NDK_HOME)
 ARM_TARGETS="armeabi armeabi-v6 armeabi-v7a"
