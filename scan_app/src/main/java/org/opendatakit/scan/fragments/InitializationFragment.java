c068cc58890ecb849dcbfc6e4d2173b177b02fca
wbrunette <wbrunette@gmail.com>
2018-08-29 13:39:20 -0700

    initial checkin of removing deprecated fragments

--- scan_app/src/main/java/org/opendatakit/scan/fragments/InitializationFragment.java
+++ scan_app/src/main/java/org/opendatakit/scan/fragments/InitializationFragment.java
@@ -1,6 +1,6 @@
 package org.opendatakit.scan.fragments;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
